a2340fd3c60a21ec536c78204450aae9e8b7fbca
Yee Yong <45476008+yeeyongnah@users.noreply.github.com>
2022-09-13 15:39:19 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -32,11 +32,15 @@ public class MyStringStackTest extends TestCase {
 	@Test
 	public void testPush() {
 		String o1 = null;  
-		MyStringStack stack = new MyStringStack();
+		/*MyStringStack stack = new MyStringStack();
 		assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
-        assertFalse (stack.isEmpty());
+        assertFalse (stack.isEmpty());*/
+		
+		MyStringStack stack = new MyStringStack();
+		stack.push (s1);
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
